34bba1d1df991353d164f66c1e93f27a1ef714c3
Harleen Singh <harleen.singh0427@gmail.com>
2022-03-31 13:04:01 +0100

    Deleted some code

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -36,24 +36,3 @@ public:
 
     //==============================================================================
     const juce::String getName() const override;
-
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    bool isMidiEffect() const override;
-    double getTailLengthSeconds() const override;
-
-    //==============================================================================
-    int getNumPrograms() override;
-    int getCurrentProgram() override;
-    void setCurrentProgram (int index) override;
-    const juce::String getProgramName (int index) override;
-    void changeProgramName (int index, const juce::String& newName) override;
-
-    //==============================================================================
-    void getStateInformation (juce::MemoryBlock& destData) override;
-    void setStateInformation (const void* data, int sizeInBytes) override;
-
-private:
-    //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Audio_Programming_Final_ProjectAudioProcessor)
-};
